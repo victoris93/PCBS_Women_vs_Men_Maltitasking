317086d609d5c8dc1f2f3f5b42000fc38d04ee44
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2021-05-06 13:54:40 +0200

    Final experiment script

--- experiment_1.py
+++ experiment_1.py
@@ -1,13 +1,10 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import glob
 from expyriment import design, control, stimuli, misc, io
 
 MALE_SEX_KEY = misc.constants.K_m
 FEMALE_SEX_KEY =  misc.constants.K_f
 
-SQUARE_RESPONSE_KEY = misc.constants.K_s # if the task is "shape"
-DIAMOND_RESPONSE_KEY = misc.constants.K_l
+SQUARE_RESPONSE_KEY = misc.constants.K_l # if the task is "shape"
+DIAMOND_RESPONSE_KEY = misc.constants.K_s
 
 TWO_CIRCLES_RESPONSE_KEY = misc.constants.K_s # if the task is "filling"
 THREE_CIRCLES_RESPONSE_KEY = misc.constants.K_l
@@ -20,18 +17,18 @@ INSTRUCTION_REMINDER_AFTER_INCORRECT_RESPONSE = 5000
 PAUSE = 500
 
 
-TRAINING_INSTRUCTIONS =  """Your task is to report the shape of the frame of the figure if it appears in the top part of the screen labeled "shape". If the frame is a square, press S on the keyboard. If it is a diamond, press L.
+TRAINING_INSTRUCTIONS =  """Your task is to report the shape of the frame of the figure if it appears in the top part of the screen labeled "shape". If the frame is a square, press L on the keyboard. If it is a diamond, press S.
     If the figure appears in the bottom part of the screen labeled "filling", you task is to report the number of circles inside of the frame. If you see 2 circles, press S on the keyboard. If you see 3 circles, press L. Let's start with a bit of training!
     
     Press the space bar to start.
     To exit the experiment, press escape."""
     
-EXPERIMENT_INSTRUCTIONS = """Report the shape of the frame of the figure if it appears in the top part of the screen labeled "shape". If the frame is a square, press S on the keyboard. If it is a diamond, press L. Report the number of circles inside of the frame, if the figure appears in the bottom part of the screen labeled "filling". If you see 2 circles, press S. If you see 3 circles, press L. Every time give an incorrect response, you will get a reminder of the instructions.
+EXPERIMENT_INSTRUCTIONS = """Report the shape of the frame of the figure if it appears in the top part of the screen labeled "shape". If the frame is a square, press L on the keyboard. If it is a diamond, press S. Report the number of circles inside of the frame, if the figure appears in the bottom part of the screen labeled "filling". If you see 2 circles, press S. If you see 3 circles, press L. Every time give an incorrect response, you will get a reminder of the instructions.
 
     Press the space bar to start.
     To exit the experiment, press escape."""
     
-REMINDER_INSTRUCTIONS = """Your task is to report the shape of the frame of the figure if it appears in the top part of the screen labeled "shape". If the frame is a square, press S on the keyboard. If it is a diamond, press L.
+REMINDER_INSTRUCTIONS = """Your task is to report the shape of the frame of the figure if it appears in the top part of the screen labeled "shape". If the frame is a square, press L on the keyboard. If it is a diamond, press S.
     If the figure appears in the bottom part of the screen labeled "filling", you task is to report the number of circles inside of the frame. If you see 2 circles, press S on the keyboard. If you see 3 circles, press L.
     
     To exit the experiment, press escape."""
@@ -50,14 +47,16 @@ def training(N_TRIALS):
 		key, rt = exp_1.keyboard.wait(duration=MAX_RESPONSE_DELAY)
 		if key == None:
 			is_correct = None
-		elif trial.get_factor('task_type') == "shape_square":
-			is_correct = (key == SQUARE_RESPONSE_KEY)
-		elif  trial.get_factor('task_type') == "shape_diamond":
-			is_correct = (key == DIAMOND_RESPONSE_KEY)
-		elif trial.get_factor('task_type') == "filling_2_circles":
-			is_correct = (key == TWO_CIRCLES_RESPONSE_KEY)
-		elif trial.get_factor('task_type') == "filling_3_circles":
-			is_correct = (key == THREE_CIRCLES_RESPONSE_KEY)
+		elif trial.get_factor('task') == "shape": #determine the task
+			if  trial.get_factor('shape') == "square": #determine correct condition
+				is_correct = (key == SQUARE_RESPONSE_KEY) #determine correct key
+			elif trial.get_factor('shape') == "diamond":
+				is_correct = (key == DIAMOND_RESPONSE_KEY)
+		elif trial.get_factor('task') == "filling":
+			if  trial.get_factor('number_of_circles') == 2:
+				is_correct = (key == TWO_CIRCLES_RESPONSE_KEY)
+			elif trial.get_factor('number_of_circles') == 3:
+				is_correct = (key == THREE_CIRCLES_RESPONSE_KEY)
 		if  is_correct == False:
 			reminder_if_incorrect_response.present()
 			exp_1.clock.wait(INSTRUCTION_REMINDER_AFTER_INCORRECT_RESPONSE)
@@ -77,24 +76,42 @@ def experiment(N_TRIALS):
 		key, rt = exp_1.keyboard.wait(duration=MAX_RESPONSE_DELAY)
 		if key == None:
 			is_correct = None
-		elif trial.get_factor('task_type') == "shape_square":
-			correct_key = 'S'
-			is_correct = (key == SQUARE_RESPONSE_KEY)
-		elif  trial.get_factor('task_type') == "shape_diamond":
-			correct_key = 'L'
-			is_correct = (key == DIAMOND_RESPONSE_KEY)
-		elif trial.get_factor('task_type') == "filling_2_circles":
-			correct_key = 'S'
-			is_correct = (key == TWO_CIRCLES_RESPONSE_KEY)
-		elif trial.get_factor('task_type') == "filling_3_circles":
-			correct_key = 'L'
-			is_correct = (key == THREE_CIRCLES_RESPONSE_KEY)
+		elif trial.get_factor('task') == "shape": #determine the task
+			if  trial.get_factor('shape') == "square": #determine correct condition
+				is_correct = (key == SQUARE_RESPONSE_KEY) #determine correct key
+				correct_key = 'L'
+				if trial.get_factor('number_of_circles') == 3: # determine corgruence condition
+					is_congruent = True #determine congruency
+				elif trial.get_factor('number_of_circles') == 2: #alternative congruency condition
+					is_congruent = False #alternative congruency
+			elif trial.get_factor('shape') == "diamond":
+				is_correct = (key == DIAMOND_RESPONSE_KEY)
+				correct_key = 'S'
+				if trial.get_factor('number_of_circles') == 2:
+					is_congruent = True
+				elif trial.get_factor('number_of_circles') == 3:
+					is_congruent = False
+		elif trial.get_factor('task') == "filling":
+			if  trial.get_factor('number_of_circles') == 2:
+				is_correct = (key == TWO_CIRCLES_RESPONSE_KEY)
+				correct_key = 'S'
+				if  trial.get_factor('shape') == "diamond":
+					is_congruent = True
+				elif trial.get_factor('shape') == "square":
+					is_congruent = False
+			elif trial.get_factor('number_of_circles') == 3:
+				is_correct = (key == THREE_CIRCLES_RESPONSE_KEY)
+				correct_key = 'L'
+				if  trial.get_factor('shape') == "square":
+					is_congruent = True
+				elif trial.get_factor('shape') == "diamond":
+					is_congruent = False
 		if  is_correct == False:
 			reminder_if_incorrect_response.present()
 			exp_1.clock.wait(INSTRUCTION_REMINDER_AFTER_INCORRECT_RESPONSE)
 			blankscreen.present()
 			exp_1.clock.wait(PAUSE)
-		exp_1.data.add([sex, i_trial, trial.get_factor('task_type'), correct_key, rt, is_correct])
+		exp_1.data.add([sex, i_trial, trial.get_factor('task'), trial.get_factor('shape'), trial.get_factor('number_of_circles'), is_congruent, correct_key, rt, is_correct])
 	
 def get_subject_sex():
 	sex_question.present()
@@ -106,46 +123,32 @@ def get_subject_sex():
 	return(sex)
 
 stimuli_block = design.Block()
-
-for pngfile in glob.glob('shape_square*.png'):
-    trial = design.Trial()
-    trial.add_stimulus(stimuli.Picture(pngfile))
-    trial.set_factor('task_type', 'shape_square')
-    stimuli_block.add_trial(trial)
-
-for pngfile in glob.glob('shape_diamond*.png'):
- 	trial = design.Trial()
- 	trial.add_stimulus(stimuli.Picture(pngfile))
- 	trial.set_factor('task_type', 'shape_diamond')
- 	stimuli_block.add_trial(trial)
-
-for pngfile in glob.glob('filling*2*.png'):
-    trial = design.Trial()
-    trial.add_stimulus(stimuli.Picture(pngfile))
-    trial.set_factor('task_type', 'filling_2_circles')
-    stimuli_block.add_trial(trial)
-    
-for pngfile in glob.glob('filling*3*.png'):
-    trial = design.Trial()
-    trial.add_stimulus(stimuli.Picture(pngfile))
-    trial.set_factor('task_type', 'filling_3_circles')
-    stimuli_block.add_trial(trial)
-
+for task in ['filling', 'shape']:
+    for shape in ['diamond', 'square']:
+        for number in [2, 3]:
+            trial = design.Trial()
+            trial.set_factor('task', task)
+            trial.set_factor('shape', shape)
+            trial.set_factor('number_of_circles', number)
+            pngname = '{}_{}_{}_circles.png'.format(task, shape, number)
+            trial.add_stimulus(stimuli.Picture(pngname))
+            stimuli_block.add_trial(trial)
+stimuli_block.shuffle_trials()
 
 blankscreen = stimuli.BlankScreen()
 sex_question = stimuli.TextScreen("One last question...", heading_size = 40, text = "What is your biological sex? Press M for male and F for female.")
 training_instructions = stimuli.TextScreen("Instructions:", heading_size = 40, text = TRAINING_INSTRUCTIONS)
 reminder_if_incorrect_response =  stimuli.TextScreen("Your response was incorrect. Instructions reminder:", heading_size = 40, text =  REMINDER_INSTRUCTIONS)   
 experiment_instructions =  stimuli.TextScreen("So, you're all set for the experiment! Instructions:", heading_size = 40, text =  EXPERIMENT_INSTRUCTIONS) 
-exp_1.data_variable_names = ["Sex", "Trial Number", "Task Type", "Correct Key", "RT", "Correct Response"]
+exp_1.data_variable_names = ["Sex", "Trial Number", "Task Type","Frame Shape", "Number of Circles","Congruent Trial", "Correct Key", "RT", "Correct Response"]
 
 kb = io.Keyboard()
 
 control.start()
 
-training(1)
+training(TRAINING_TRIALS)
 
-experiment(5)
+experiment(EXPERIMENT_TRIALS)
 
 control.end()
 
